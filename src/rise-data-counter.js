b4df061728b6fede94c127eef790e514601adcd7
Francisco Vallarino <fjvallarino@users.noreply.github.com>
2019-10-15 17:29:30 -0300

    Add attributes and observers (#3)
    * Add attributes and observers
    
    * Do not observe type and refresh attributes
    
    * Temporarily lower coverage threshold until we have code to test

--- src/rise-data-counter.js
+++ src/rise-data-counter.js
@@ -11,11 +11,47 @@ export default class RiseDataCounter extends RiseElement {
 
   static get properties() {
     return {
+      /**
+       * The direction this instance should be configured to count for. Valid values are "down" and "up".
+       */
       type: {
         type: String,
         value: "down"
+      },
+      /**
+       * The date value to count up from or down to. Valid format is YYYY-MM-DD.
+       */
+      date: {
+        type: String
+      },
+      /**
+       * The time value to count up from or down to. Valid format is HH:mm.
+       */
+      time: {
+        type: String
+      },
+      /**
+       * The message to display when the countdown is complete. Only used for a type configuration of down.
+       */
+      completion: {
+        type: String
+      },
+      /**
+       * The rate at which the component should provide a new timestamp value. Unit is seconds.
+       */
+      refresh: {
+        type: Number,
+        value: 1
       }
     };
+  }
+
+  // Each item of observers array is a method name followed by
+  // a comma-separated list of one or more dependencies.
+  static get observers() {
+    return [
+      "_reset(date, time, completion)"
+    ];
   }
 
   constructor() {
@@ -24,6 +60,9 @@ export default class RiseDataCounter extends RiseElement {
     this._setVersion( version );
   }
 
+  _reset() {
+
+  }
 }
 
 customElements.define("rise-data-counter", RiseDataCounter);
